8191395944b1c7eb53a5c154b8a578db16a9e321
David Landry <davidlandry93@gmail.com>
2016-11-22 17:49:22 -0500

    Finished the section about initial estimates

--- robmob/icp_orig.py
+++ robmob/icp_orig.py
@@ -54,32 +54,21 @@ def nearest_neighbor(src, dst):
     indecies = np.zeros(src.shape[0], dtype=np.int)
     distances = np.zeros(src.shape[0])
     for i, s in enumerate(src):
-        distances_mat = np.linalg.norm(dst[:] - s, axis=1)
-        indecies[i] = np.argmin(distances_mat)
-        distances[i] = distances_mat[indecies[i]]
-        
+        min_dist = np.inf
+        for j, d in enumerate(dst):
+            dist = np.linalg.norm(s-d)
+            if dist < min_dist:
+                min_dist = dist
+                indecies[i] = j
+                distances[i] = dist
     return distances, indecies
 
-def homogeneous_copy_of_pcl(pcl):
-    '''
-    Input:
-        pcl: Nx3 or Nx4 numpy array of 3D points.
-    Output:
-        A deep copy of the point cloud, in homogeneous coordinates.
-        If the input matrix had 4 columns, it is returned as is.
-    '''
-    if pcl.shape[1] == 4:
-        return np.copy(pcl.T)
-    elif pcl.shape[1] == 3:
-        copy_of_pcl = np.copy(pcl)
-        return np.concatenate((copy_of_pcl.T, np.ones((1, copy_of_pcl.shape[0]))), axis=0)
-
-def icp(A, B, init_pose=None, max_iterations=50, tolerance=0.001):
+def icp(A, B, init_pose=None, max_iterations=20, tolerance=0.001):
     '''
     The Iterative Closest Point method
     Input:
-        A: Nx3 or Nx4 numpy array of source 3D points
-        B: Nx3 or Nx4 numpy array of destination 3D point
+        A: Nx3 numpy array of source 3D points
+        B: Nx3 numpy array of destination 3D point
         init_pose: 4x4 homogeneous transformation
         max_iterations: exit algorithm after max_iterations
         tolerance: convergence criteria
@@ -88,9 +77,11 @@ def icp(A, B, init_pose=None, max_iterations=50, tolerance=0.001):
         distances: Euclidean distances (errors) of the nearest neighbor
     '''
 
-    # Convert the points to homogeneous coordinates.
-    src = homogeneous_copy_of_pcl(A)
-    dst = homogeneous_copy_of_pcl(B)
+    # make points homogeneous, copy them so as to maintain the originals
+    src = np.ones((4,A.shape[0]))
+    dst = np.ones((4,B.shape[0]))
+    src[0:3,:] = np.copy(A.T)
+    dst[0:3,:] = np.copy(B.T)
 
     # apply the initial pose estimation
     if init_pose is not None:
@@ -109,13 +100,12 @@ def icp(A, B, init_pose=None, max_iterations=50, tolerance=0.001):
         src = np.dot(T, src)
 
         # check error
-        mean_error = np.average(distances)
-        print('Mean distance between associated points: {}'.format(mean_error))
+        mean_error = np.sum(distances) / distances.size
         if abs(prev_error-mean_error) < tolerance:
             break
         prev_error = mean_error
 
     # calculcate final tranformation
-    T,_,_ = best_fit_transform(A[:,0:3], src[0:3,:].T)
+    T,_,_ = best_fit_transform(A, src[0:3,:].T)
 
     return T, distances
